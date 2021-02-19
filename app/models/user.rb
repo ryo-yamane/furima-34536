d7c885b23300811c302dd60cfa95c64388a5eb5d
ryo-yamane <78146211+ryo-yamane@users.noreply.github.com>
2021-02-19 18:20:14 +0900

    ログイン機能追加（バリデーション設定済）

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :nickname, presence:true
+  with_options presence:true,format:{with:/\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+  validates :first_name 
+  validates :last_name 
+  end
 
+  with_options presence:true,format:{with:/\A[ァ-ヶ]+\z/, message: "カタカナでお願いします"} do
+  validates :first_name_kana
+  validates :last_name_kana
+  end
   
-end
+  with_options  format:{with:/\A(.?*?[a-z])(?=.*?[\d])[a-z\d]+\z/i} do
+   validates :password 
+  end
+  
+  validates :birthday, presence:true
+ end
