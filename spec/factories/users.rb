e98655bb31a88fe365d4c679158507bf430dd118
ryo-yamane <78146211+ryo-yamane@users.noreply.github.com>
2021-02-23 02:04:46 +0900

    指摘箇所修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,7 +6,7 @@ FactoryBot.define do
 
     nickname      {Faker::Name.initials(number: 2)}
     email         {Faker::Internet.free_email}
-    password      {Faker::Internet.password(min_length: 6)}
+    password      {Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1)}
     password_confirmation {password}    
     first_name    {person.first.kanji}
     last_name     {person.last.kanji}
