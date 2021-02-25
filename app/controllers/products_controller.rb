a80ac1df77c4eef574ae26246b26e229739a1758
ryo-yamane <78146211+ryo-yamane@users.noreply.github.com>
2021-02-25 11:14:26 +0900

    削除機能追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit ,:update]
-  before_action :set_product, only: [:show, :edit, :update]
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
   
   def index
     @products = Product.all.order('created_at DESC')
@@ -35,6 +35,16 @@ class ProductsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    if @product.user == current_user
+      @product.destroy
+      redirect_to root_path
+    else
+      ewnder :edit
+    end
+
   end
 
   private
